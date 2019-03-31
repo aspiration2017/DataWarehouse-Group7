028878c42d4d79217efc0a0c3d074e216b7755b2
unknown <nhoktuyenpro@gmail.com>
2019-03-31 16:05:56 +0700

    generate class connection for control & staging db

---
+++ src/connection/StagingConnection.java
@@ -0,0 +1,42 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class StagingConnection {
+	private Connection cnn;
+
+	public StagingConnection() {
+		super();
+		String host = "cakkun.mysql.database.azure.com";
+		String dbName = "staging";
+		String username = "dwg7@cakkun";
+		String password = "D@tawarehouse2019";
+		String url = String.format("jdbc:mysql://%s/%s", host, dbName);
+		Properties properties = new Properties();
+		properties.setProperty("user", username);
+		properties.setProperty("password", password);
+		properties.setProperty("useSSL", "true");
+		properties.setProperty("verifyServerCertificate", "true");
+		properties.setProperty("requireSSL", "false");
+		properties.setProperty("zeroDateTimeBehavior", "CONVERT_TO_NULL");
+		properties.setProperty("serverTimezone", "UTC");
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); // new driver for connector/J 8.0.15
+			cnn = DriverManager.getConnection(url, properties);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getCnn() {
+		return cnn;
+	}
+	
+	
+	
+}
