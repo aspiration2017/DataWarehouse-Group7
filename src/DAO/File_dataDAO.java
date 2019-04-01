9c4b8bef67bce3b539bec9dbc306c7847d7b376b
unknown <nhoktuyenpro@gmail.com>
2019-04-01 21:26:20 +0700

    generate some necessary method for DAO

--- src/DAO/File_dataDAO.java
+++ src/DAO/File_dataDAO.java
@@ -1,43 +1,131 @@
 package DAO;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import model.File_data;
 
 public class File_dataDAO {
-	Connection cnn;
-	
-	public File_dataDAO(Connection cnn) {
-		super();
-		this.cnn = cnn;
-	}
 
-	public List<File_data> loadFileNotDownloadYet() {
+	/**
+	 * load file with status = 0
+	 * 
+	 * @param cnn
+	 * @return
+	 * @throws SQLException
+	 */
+	public static List<File_data> loadFileNotDownloadYet(Connection cnn) throws SQLException {
 		List<File_data> result = new ArrayList<>();
-		// TODO load files from db control (table file_data_log) with status : 0
-
+		String sql = "select * from file_data_log where status = 0";
+		Statement stmt = cnn.createStatement();
+		ResultSet rs = stmt.executeQuery(sql);
+		while (rs.next()) {
+			result.add(new File_data(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getString(5),
+					rs.getInt(6), rs.getInt(8)));
+		}
+		rs.close();
+		stmt.close();
 		return result;
 	}
-	
-	public List<File_data> loadDownloadedFile() {
-		List<File_data> result = new ArrayList<>();
-		// TODO load files from db control (table file_data_log) with status : 1
 
+	/**
+	 * load file with status = 1
+	 * 
+	 * @param cnn
+	 * @return
+	 * @throws SQLException
+	 */
+	public static List<File_data> loadDownloadedFile(Connection cnn) throws SQLException {
+		List<File_data> result = new ArrayList<>();
+		String sql = "select * from file_data_log where status = 1";
+		Statement stmt = cnn.createStatement();
+		ResultSet rs = stmt.executeQuery(sql);
+		while (rs.next()) {
+			result.add(new File_data(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getString(5),
+					rs.getInt(6), rs.getInt(8)));
+		}
+		rs.close();
+		stmt.close();
 		return result;
 	}
-	
-	public void updateStatus(String idFile, String stt) {
-		// TODO update status file
+
+	/**
+	 * update status file when download || extract file into staging
+	 * 
+	 * @param cnn
+	 * @param idFile
+	 * @param stt
+	 * @throws SQLException
+	 */
+	public static void updateStatus(Connection cnn, int idFile, int stt) throws SQLException {
+		String sql = "UPDATE file_data_log SET status = ? where id = ?";
+		PreparedStatement stmt = cnn.prepareStatement(sql);
+		stmt.setInt(1, stt);
+		stmt.setInt(2, idFile);
+		stmt.executeUpdate();
+		stmt.close();
+	}
+
+	public static void updateLocalPathFile(Connection cnn, int idFile, String path) throws SQLException {
+		String sql = "UPDATE file_data_log SET local_path = ? where id = ?";
+		PreparedStatement stmt = cnn.prepareStatement(sql);
+		stmt.setString(1, path);
+		stmt.setInt(2, idFile);
+		stmt.executeUpdate();
+		stmt.close();
+	}
+
+	public static void updateLoadedRows(Connection cnn, int idFile, int loaded_rows) throws SQLException {
+		String sql = "UPDATE file_data_log SET loaded_rows = ? where id = ?";
+		PreparedStatement stmt = cnn.prepareStatement(sql);
+		stmt.setInt(1, loaded_rows);
+		stmt.setInt(2, idFile);
+		stmt.executeUpdate();
+		stmt.close();
 	}
 	
-	public void updateLocalPathFile(String id, String path) {
-		// TODO update file's localpath
+	/**
+	 * load file from local to staging database
+	 * @param cnn
+	 * @param file
+	 * @return number of loaded rows
+	 * @throws IOException
+	 */
+	public static int loadFileFromLocalToStaging(Connection cnn, File_data file) throws IOException, SQLException {
+		int loaded_rows = 0;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file.localPath), "utf-8"));
+		int numberOfFields = file.getNumberOfFields();
+		StringBuilder value = new StringBuilder();
+		for (int i = 0; i < numberOfFields; i++) {
+			if (i == numberOfFields-1)
+				value.append("?");
+			else
+				value.append("?,");
+		}
+		StringTokenizer st;
+		String sql = "INSERT INTO "+ file.host.des_table+file.host.fields +" VALUES("+value.toString()+")";
+		PreparedStatement stmt = cnn.prepareStatement(sql);
+		String line = "";
+		while ((line = reader.readLine()) != null) {
+			st = new StringTokenizer(line, file.host.delimiter);
+			for (int i = 1; i <= numberOfFields; i++) {
+				stmt.setString(i, st.nextToken());
+			}
+			stmt.executeUpdate();
+			loaded_rows++;
+		}
+		stmt.close();
+		reader.close();
+		return loaded_rows;
 	}
-	
-	public void updateExtractedRows(String line) {
-		// TODO count number of row was loaded into staging, update it into table: file_data_log
-	}
-	
 }
--- src/DAO/HostDAO.java
+++ src/DAO/HostDAO.java
@@ -1,17 +1,30 @@
 package DAO;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import model.Host;
 
 public class HostDAO {
-	
-	public List<Host> loadHost(Connection cnn) {
+
+	public static List<Host> loadHost(Connection cnn) throws SQLException {
 		List<Host> result = new ArrayList<>();
-		// TODO load host from db control (table host_config)
-		
+		String query = "select * from host_config";
+		Statement stmt = null;
+		ResultSet rs = null;
+		stmt = cnn.createStatement();
+		rs = stmt.executeQuery(query);
+
+		while (rs.next()) {
+			result.add(new Host(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(7),
+					rs.getString(8), rs.getString(10)));
+		}
+		rs.close();
+		stmt.close();
 		return result;
 	}
 }
