e6fdb20cb9a29b6ee994424bfa0b055ce2a37d06
unknown <nhoktuyenpro@gmail.com>
2019-04-17 19:33:51 +0700

    add class to get connection on dataWarehouse & dataMart | and class DAO to use them

---
+++ src/connection/WarehouseConnection.java
@@ -0,0 +1,39 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class WarehouseConnection {
+	private Connection cnn;
+
+	public WarehouseConnection() {
+		super();
+		String host = "cakkun.mysql.database.azure.com";
+		String dbName = "datawarehouse";
+		String username = "dwg7@cakkun";
+		String password = "D@tawarehouse2019";
+		String url = String.format("jdbc:mysql://%s/%s", host, dbName);
+		Properties properties = new Properties();
+		properties.setProperty("user", username);
+		properties.setProperty("password", password);
+		properties.setProperty("useSSL", "true");
+		properties.setProperty("verifyServerCertificate", "true");
+		properties.setProperty("requireSSL", "false");
+		properties.setProperty("zeroDateTimeBehavior", "CONVERT_TO_NULL");
+		properties.setProperty("serverTimezone", "UTC");
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); // new driver for connector/J 8.0.15
+			cnn = DriverManager.getConnection(url, properties);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getCnn() {
+		return cnn;
+	}
+}
